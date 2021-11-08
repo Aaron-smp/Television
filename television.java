68f0e62076917c8f492aad55d5c6e8615ec7b210
Aarón Pérez Carreño <aapecargame@gmail.com>
2021-11-08 14:24:32 +0100

    01- Commit inicial

---
+++ television.java
@@ -0,0 +1,65 @@
+public class television {
+    //marca de la television
+    private String marca;
+    //tamaño en pulgadas de la television
+    private int volumen;
+    //si esta la television encendida o apagada
+    private boolean teleEncendida;
+
+    public television(String marcaTV, int volumenTV){
+        marca = marcaTV;
+        volumen = volumenTV;
+        teleEncendida = true;
+    }
+
+    public String getMarca(){
+        return marca;
+    }
+
+    public int getVolumen(){
+        return volumen;
+    }
+
+    public boolean getEncendida(){
+        return teleEncendida;
+    }
+
+    public void setMarca(String newMarca){
+        marca = newMarca;
+    }
+
+    public void setSubirVolumen(int subirVolumen){
+        volumen += subirVolumen;
+    }
+
+    public void botonApagarEncender(){
+
+        if(teleEncendida == true){
+
+            teleEncendida = false;
+        }
+        else{
+            teleEncendida = true;
+        }
+
+    }
+
+    public void infoTV(){
+        if(teleEncendida == true){
+            System.out.println("La marca de la television es " + marca + "|" + "Tiene " + volumen + " de volumen" + "| La television si está encendida" );
+        }
+        else {
+            System.out.println("La marca de la television es }" + marca + "|" + "Tiene " + volumen + " de volumen" + "| La television está apagada" );
+        }
+    }
+
+    public void estadoTV(){
+        System.out.println("La television esta ");
+        if(teleEncendida == true){
+            System.out.println("encendida");
+        }
+        else{
+            System.out.println("apagada");
+        }
+    }
+}
